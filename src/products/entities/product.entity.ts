42b395e948778130bb022b1dcdc74a50e01cd5a4
joao paulo <jhonysp37@gmail.com>
2024-10-25 16:16:30 -0300

    Product Entity

---
+++ src/products/entities/product.entity.ts
@@ -0,0 +1,35 @@
+import { Column, CreateDateColumn, DeleteDateColumn, OneToOne, PrimaryGeneratedColumn, UpdateDateColumn } from "typeorm";
+
+
+export class ProductEntity {
+    @PrimaryGeneratedColumn('uuid')
+    id: string
+
+    @Column({ name: 'user_id' })
+    userID: string
+
+    @Column({ name: 'name', nullable: false, length: 100 })
+    name: string
+
+    @Column({ name: 'description', nullable: false })
+    description: string
+
+    @Column({ name: 'category', nullable: false })
+    category: string
+
+    @Column({ name: 'quantity', nullable: false })
+    quantity: number
+
+    @Column({ name: 'price', nullable: false })
+    price: number
+
+    @CreateDateColumn({ name: 'created_at' })
+    createdAt: string
+
+    @UpdateDateColumn({ name: 'updated_at' })
+    updatedAt: string
+
+    @DeleteDateColumn({ name: 'deleted_at' })
+    deletedAt: string
+
+}
