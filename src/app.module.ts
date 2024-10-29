fcbca0d6be68146e91d8cae550c5fc3cd3b2c37b
joao paulo <jhonysp37@gmail.com>
2024-10-29 09:19:42 -0300

    Estrutura User - entity user

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { ProductModule } from './products/product.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { config } from './ormconfig';
+import { UserModule } from './users/user.module';
 
 @Module({
-  imports: [ProductModule, TypeOrmModule.forRoot(config)],
+  imports: [ProductModule, UserModule, TypeOrmModule.forRoot(config)],
   controllers: [],
   providers: [],
 })
--- src/products/product.service.ts
+++ src/products/product.service.ts
@@ -61,4 +61,6 @@ export class ProductService {
 
         return this.productRepository.remove(productToDelete)
     }
+
+
 }
