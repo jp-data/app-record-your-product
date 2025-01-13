4e54812746c3e7dfa6689f10f16cf742a943668f
joao paulo <jhonysp37@gmail.com>
2025-01-13 16:11:43 -0300

    Criar pedido - consumo api

--- front-end/src/api-requisitions/create-order.ts
+++ front-end/src/api-requisitions/create-order.ts
@@ -4,9 +4,15 @@ import { RegisterOrderForm } from '../pages/app/sales/cart-new-sale'
 
 
 export async function createOrders(orderData: RegisterOrderForm) {
+    const token = localStorage.getItem('token')
+
+    if (!token) {
+        throw new Error("Usuário não autenticado")
+    }
     const response = await api.post('/orders', orderData, {
         headers: {
-            'Content-Type': 'application/json'
+            'Content-Type': 'application/json',
+            Authorization: `Bearer ${token}`
         }
     })
     return response.data
