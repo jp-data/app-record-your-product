5e89c4e9590aa2c1095fd09f75c9631506fd20e6
joao paulo <jhonysp37@gmail.com>
2025-02-05 10:09:59 -0300

    Verificando variaveis de ambiente

--- back-end/src/database/ormconfig.ts
+++ back-end/src/database/ormconfig.ts
@@ -5,11 +5,11 @@ import * as path from 'path'
 const environment = process.env.NODE_ENV || 'development'
 const envFile = path.resolve(__dirname, `../../src/database/${environment}.env`);
 dotenv.config({ path: envFile })
-// console.log("DB_HOST:", process.env.DB_HOST);
-// console.log("DB_PORT:", process.env.DB_PORT);
-// console.log("DB_USERNAME:", process.env.DB_USERNAME);
-// console.log("DB_PASSWORD:", process.env.DB_PASSWORD);
-// console.log("DB_NAME:", process.env.DB_NAME);
+console.log("DB_HOST:", process.env.DB_HOST);
+console.log("DB_PORT:", process.env.DB_PORT);
+console.log("DB_USERNAME:", process.env.DB_USERNAME);
+console.log("DB_PASSWORD:", process.env.DB_PASSWORD);
+console.log("DB_NAME:", process.env.DB_NAME);
 
 if (!process.env.DB_HOST || !process.env.DB_PORT || !process.env.DB_USERNAME || !process.env.DB_PASSWORD || !process.env.DB_NAME) {
     throw new Error(`As variáveis de ambiente para o ambiente ${environment} não estão configuradas corretamente.`);
