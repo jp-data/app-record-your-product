0af013eb3ce0db3a4c8dd2aa81db14ed6126ce2c
joao paulo <jhonysp37@gmail.com>
2025-02-05 10:30:09 -0300

    Teste - variaveis de ambiente em producao

--- back-end/src/database/ormconfig.ts
+++ back-end/src/database/ormconfig.ts
@@ -2,15 +2,11 @@ import { DataSource, DataSourceOptions } from "typeorm";
 import * as dotenv from 'dotenv'
 import * as path from 'path'
 
-console.log(process.env.NODE_ENV)
+
 const environment = process.env.NODE_ENV || 'development'
-const envFile = path.resolve(__dirname, `../../src/database/${environment}.env`);
+const envFile = path.resolve(__dirname, `/home/sba-suporte/app-record-your-product/back-end/src/database/${environment}.env`);
 dotenv.config({ path: envFile })
-console.log("DB_HOST:", process.env.DB_HOST);
-console.log("DB_PORT:", process.env.DB_PORT);
-console.log("DB_USERNAME:", process.env.DB_USERNAME);
-console.log("DB_PASSWORD:", process.env.DB_PASSWORD);
-console.log("DB_NAME:", process.env.DB_NAME);
+
 
 if (!process.env.DB_HOST || !process.env.DB_PORT || !process.env.DB_USERNAME || !process.env.DB_PASSWORD || !process.env.DB_NAME) {
     throw new Error(`As variáveis de ambiente para o ambiente ${environment} não estão configuradas corretamente.`);
