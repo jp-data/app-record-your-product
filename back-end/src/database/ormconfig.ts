b485978615e9d046c023c43e48fa9cfd6d82c5fe
joao paulo <jhonysp37@gmail.com>
2025-02-06 13:54:10 -0300

    erro prod - conexao banco

--- back-end/src/database/ormconfig.ts
+++ back-end/src/database/ormconfig.ts
@@ -20,7 +20,9 @@ const config: DataSourceOptions = {
     password: process.env.DB_PASSWORD,
     database: process.env.DB_NAME,
     synchronize: environment === 'production' ? false : true,
-    entities: [__dirname + '/../**/*.entity{.ts,.js}'],
+    entities: process.env.NODE_ENV === 'production' ? [__dirname + '/../entities/*.js']
+        :
+        [__dirname + '/../**/*.entity{.ts,.js}'],
     migrations: [__dirname + '/../migrations/*{.ts,.js}'],
     migrationsRun: environment === 'production',
     logging: environment === 'development'
