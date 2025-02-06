6d8913333c9d4a5756b349ee0f4b847334852426
joao paulo <jhonysp37@gmail.com>
2025-02-06 14:28:18 -0300

    erro prod - conexao banco

--- back-end/src/main.ts
+++ back-end/src/main.ts
@@ -4,7 +4,9 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, {
+    logger: ['log', 'error', 'warn', 'debug', 'verbose']
+  });
   app.enableCors({
     origin: 'http://localhost:5173',
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
