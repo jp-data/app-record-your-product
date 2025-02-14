39e51369b29195d72566092da99cdecfc52bfd28
joao paulo <jhonysp37@gmail.com>
2025-02-14 17:12:46 -0300

    alteraçao - order entitiy created_attype

--- back-end/src/database/migrations/1739563881018-alterOrderCretedAtType.ts
+++ back-end/src/database/migrations/1739563881018-alterOrderCretedAtType.ts
@@ -1,11 +1,11 @@
 import { MigrationInterface, QueryRunner } from "typeorm";
 
-export class AlterOrderCretedAt1739563468053 implements MigrationInterface {
+export class AlterOrderCretedAtType1739563881018 implements MigrationInterface {
 
     public async up(queryRunner: QueryRunner): Promise<void> {
         await queryRunner.query(`
             ALTER TABLE orders
-            ALTER COLUMN created_at SET DEFUALT NOW(),
+            ALTER COLUMN created_at SET DEFAULT NOW(),
             ALTER COLUMN created_At SET NOT NULL,
             ALTER COLUMN created_at TYPE TIMESTAMP
             `)
@@ -17,7 +17,6 @@ export class AlterOrderCretedAt1739563468053 implements MigrationInterface {
             ALTER COLUMN created_at DROP DEFAULT,
             ALTER COLUMN created_at DROP NOT NULL;
         `);
-
     }
 
 }
