ddef4f0e89c0ca5b39b9d2e18c0f9f620ef9ab95
joao paulo <jhonysp37@gmail.com>
2025-02-18 08:43:54 -0300

    vite config - tentativa erro MIME TYPE

--- front-end/vite.config.ts
+++ front-end/vite.config.ts
@@ -3,9 +3,6 @@ import react from '@vitejs/plugin-react-swc'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '',
   plugins: [react()],
-  build: {
-    outDir: 'dist'
-  }
+  base: '/dist'
 })
